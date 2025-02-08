b3386e4430cf69f09b38f7720ccc55e689a4562a
misagh <misaghhasani.c@gmail.com>
2025-02-08 14:18:25 +0330

    fanction

---
+++ map.js
@@ -0,0 +1,22 @@
+//argiument
+//parametr
+
+let mim = [13,22,43,23,55];
+let mim2 = mim.map(x => x-1);
+console.log(mim2);
+let mim3 = ["Misagh","Mah","mmd","amin"];
+let mim4 = mim3.map(x => x.toLowerCase());
+console.log(mim4);
+//نمونه ای از های اوردر فانگشن:مثال مپ که تغییراتی روی ارایه ما انجام میدهد
+
+
+let mim5 = [12,14,14,52];
+let mim6 = mim5.length();
+//برای شمارنده هر حرف یه المان
+//  indexOf charAt substring , trim 
+
+
+
+
+
+
