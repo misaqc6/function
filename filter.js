ce93e40e283de64a4675b05fb46e6ff7b4bee45a
misagh <misaghhasani.c@gmail.com>
2025-02-09 12:02:48 +0330

    Practice with M

--- filter.js
+++ filter.js
@@ -3,3 +3,22 @@ let mim1 = mim.filter(x => x %2===0);
 console.log(mim1);
 
 // عدادی که ضرب در 2 تقسیم بر 3 =15 میشوند را خروجی نشان بدهد
+
+let li = [10,15,19,20,44,50,55,12,34,56,788,193,22.5];
+let mim2 = li.filter(x => x*2/3===15);
+console.log(mim2);
+
+let ls =[12,13,14,15,6,19,9,5];
+let small = ls.filter(x => x <10);
+console.log("number small = ",small);
+let big = ls.filter(x => x >10);
+console.log("number big =",big);
+
+
+let lf =[12,13,14,15,6,19,9,5];
+if (lf.filter(x => x <= 20 && x >= 0)){
+    console.log("number is true " , lf);
+}else {
+    console.log("number is error")
+}
+
--- reduce.js
+++ reduce.js
@@ -4,3 +4,17 @@ console.log(mim1);
 
 
 // یک ارایه تشکیل دهید و همه عدد ها را جمع کنید
+
+
+let li = [12,13,14,16,88,75,3,4,5,22,622];
+let jm = li.reduce((a,b)=> a*b)
+console.log(jm)
+
+
+let ls = [12,13,14,16,88,75,22,4,5,22,62];
+let taf = ls.reduce((a,b)=> a-b,0)
+console.log(taf)
+
+let lf = [2,132,3,3,2,2,1,2,222];
+let tagh = lf.reduce((a,b)=> a/b)
+console.log(tagh)
